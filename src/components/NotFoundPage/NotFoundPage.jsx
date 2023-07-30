d20636d3fbf81d54b34548a81a312a730e33fba9
Andrew B <unbugster@gmail.com>
2023-07-30 17:56:44 +0300

    Fix NotFoundPage header

--- src/components/NotFoundPage/NotFoundPage.jsx
+++ src/components/NotFoundPage/NotFoundPage.jsx
@@ -5,7 +5,7 @@ const NotFoundPage = () => {
   return (
     <section className="not-found">
       <div className="not-found__container">
-        <h2 className="not-found__title">404</h2>
+        <h1 className="not-found__title">404</h1>
         <p className="not-found__text">Страница не найдена</p>
       </div>
       <Link className="not-found__link" to="/">
