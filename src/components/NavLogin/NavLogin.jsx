68437045f1f04b13eeb076e41f4dd6d438d2b9d5
Andrew B <unbugster@gmail.com>
2023-07-31 22:20:13 +0300

    Fix NavLogin validation

--- src/components/NavLogin/NavLogin.jsx
+++ src/components/NavLogin/NavLogin.jsx
@@ -1,16 +1,15 @@
-import { Link } from "react-router-dom";
 import { Button } from "../Button";
 import "./NavLogin.css";
 
 const NavLogin = () => {
   return (
     <div className="login-menu">
-      <Link to="/signup">
-        <Button transparent>Регистрация</Button>
-      </Link>
-      <Link to="/signin">
-        <Button>Войти</Button>
-      </Link>
+      <Button className="login-menu__link" to="/signup" transparent>
+        Регистрация
+      </Button>
+      <Button className="login-menu__link" to="/signin">
+        Войти
+      </Button>
     </div>
   );
 };
