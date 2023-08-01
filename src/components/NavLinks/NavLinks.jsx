7d22417bb28165eacb8f4621c684f4ed87b2b4f8
Andrew B <unbugster@gmail.com>
2023-08-01 22:17:14 +0300

    Fix NavLinks semantic

--- src/components/NavLinks/NavLinks.jsx
+++ src/components/NavLinks/NavLinks.jsx
@@ -14,45 +14,51 @@ const NavLinks = (props) => {
   const btnCls = `button_header-menu nav__link`;
 
   return (
-    <div className={classNames("nav", cls)}>
+    <nav className={classNames("nav", cls)}>
       <div className="nav__container">
-        <div className={classNames("nav__links")}>
-          <Button
-            to="/"
-            onClick={handleLinkClick}
-            transparent
-            hidden
-            className={btnCls}
-            activeCls={activeCls}
-          >
-            Главная
-          </Button>
-          <Button
-            to="/movies"
-            onClick={handleLinkClick}
-            transparent
-            className={btnCls}
-            activeCls={activeCls}
-          >
-            Фильмы
-          </Button>
-          <Button
-            to="/saved-movies"
-            onClick={handleLinkClick}
-            transparent
-            className={btnCls}
-            activeCls={activeCls}
-          >
-            Сохранённые фильмы
-          </Button>
-        </div>
+        <ul className={classNames("nav__links")}>
+          <li>
+            <Button
+              to="/"
+              onClick={handleLinkClick}
+              transparent
+              hidden
+              className={btnCls}
+              activeCls={activeCls}
+            >
+              Главная
+            </Button>
+          </li>
+          <li>
+            <Button
+              to="/movies"
+              onClick={handleLinkClick}
+              transparent
+              className={btnCls}
+              activeCls={activeCls}
+            >
+              Фильмы
+            </Button>
+          </li>
+          <li>
+            <Button
+              to="/saved-movies"
+              onClick={handleLinkClick}
+              transparent
+              className={btnCls}
+              activeCls={activeCls}
+            >
+              Сохранённые фильмы
+            </Button>
+          </li>
+        </ul>
 
         <UserMenu
           isBurgerActive={isBurgerActive}
           setIsBurgerActive={setIsBurgerActive}
         />
       </div>
-    </div>
+    </nav>
   );
 };
 
