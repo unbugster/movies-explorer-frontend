f7cd6b056d8d0358c3999c1d0b1dc286a634bd9b
Andrew B <unbugster@gmail.com>
2023-08-05 19:31:23 +0300

    Add button_type_more logic

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -2,21 +2,25 @@ import "./MoviesCardList.css";
 import { MoviesCard } from "../MoviesCard";
 import useResize from "../../hooks/useResize.js";
 import { Button } from "../Button";
-import { useLocation } from "react-router-dom";
+// import { useLocation } from "react-router-dom";
 import { useEffect, useMemo, useState } from "react";
 
 const MoviesCardList = (props) => {
   const { movies } = props;
   const size = useResize();
-  const location = useLocation();
+  // const location = useLocation();
   const [moviesToAdd, setMoviesToAdd] = useState(0);
+
+  const handleClick = () => {
+    setMoviesToAdd((prev) => prev + (size.width >= 1280 ? 4 : 2));
+  };
 
   useEffect(() => {
     setMoviesToAdd(0);
   }, [movies]);
 
   const moviesToRender = useMemo(() => {
-    const countToRender = size.width < 768 ? 5 : size.width < 1280 ? 8 : 12;
+    const countToRender = size.width < 768 ? 5 : size.width < 1280 ? 8 : 16;
 
     return movies.slice(0, countToRender + moviesToAdd);
   }, [movies, moviesToAdd, size]);
@@ -29,10 +33,13 @@ const MoviesCardList = (props) => {
         })}
       </ul>
 
-      {location.pathname === "/saved-movies" ? (
-        <div className="movies-placeholder"></div>
-      ) : (
-        <Button className="button_type_more" btnType="button" transparent>
+      {movies.length > moviesToRender.length && (
+        <Button
+          className="button_type_more"
+          btnType="button"
+          transparent
+          onClick={handleClick}
+        >
           Еще
         </Button>
       )}
